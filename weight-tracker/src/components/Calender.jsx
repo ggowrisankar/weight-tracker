336a52d9fb760865cdd43e5492eb0dcd1c39cb74
Gowri Sankar <gowrisankar9817@gmail.com>
2025-08-26 18:31:11 +0530

    Added Weekdays header logic and reconfigured daysArray to incorporate the null values and the actual days

--- weight-tracker/src/components/Calender.jsx
+++ weight-tracker/src/components/Calender.jsx
@@ -3,24 +3,60 @@ function Calendar () {
   const year = today.getFullYear();
   const month = today.getMonth();
   const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  const daysInMonth = new Date(year, month+1, 0).getDate(); //using the last day of next month to get the total days
+  const weekDays = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"];
 
-  const days = Array.from({length:daysInMonth}, (_,i) => i+1);
+  const daysInMonth = new Date(year, month+1, 0).getDate(); //using day "0" of next month gives last day of current month
+  const firstDay = new Date(year, month, 1).getDay();
+  const firstDayofMonth =  (firstDay + 6) % 7; //using it to make Mon-Sun as indices 0-6
+
+  const daysArray = [
+    ...Array(firstDayofMonth).fill(null),                  //empty slots until the first day
+    ...Array.from({length:daysInMonth}, (_,i) => i+1)     //creating an array starting from 1 to total no of days
+  ];   
 
   return(
     <div>
-      <h2>{monthName[month]}, {year}</h2>
+      {/* Month + Year title */}
+      {/* <h2>{monthName[month]}, {year}</h2> */}
+      <h2>
+        {today.toLocaleString("default", {month: "long"})} {year}
+      </h2>
 
-      <div style={{display: "grid", gridTemplateColumns: "repeat(7, 1fr)", gap: "5px"}}>
-        {days.map((day) => (
+      {/* Weekday header */}
+      <div style={{
+        display: "grid", 
+        gridTemplateColumns: "repeat(7, 1fr)", 
+        marginBottom: "10px",
+        fontWeight: "bold",
+        textAlign: "center"
+        }}
+      >
+        {weekDays.map((day) => (
+          <div key={day}>    
+            {day}
+          </div>
+        ))}
+      </div>
+
+      {/* Calendar days */}
+      <div style={{
+          display: "grid", 
+          gridTemplateColumns: "repeat(7, 1fr)", 
+          textAlign: "center",
+          gap: "5px"
+        }}
+      >
+        {daysArray.map((day, index) => (
           <div
-            key={day}
+            key={`${year}-${month + 1}-${day || `empty-${index}`}`} //unique key value yyyy-mm-dd / yyyy-mm-empty-0,1,2...
             style={{
               border: "1px solid gray",
               padding: "10px",
-              textAlign: "center"
-            }}>
-            {day}
+              backgroundColor: day ? "black" : "#f5f5f523", // empty slots = gray
+              color: "white"
+            }}
+          >
+            {day || ""}
           </div>
         ))}
       </div>
