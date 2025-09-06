7de45194a6bd986d3412993e101c0f44e3ac4f9c
Gowri Sankar <gowrisankar9817@gmail.com>
2025-09-06 20:18:40 +0530

    Configured dotenv and added env file (port and api key) to protect sensitive infos and to use available ports according. Also added gitignore to avoid env files from pushing.

--- weather-proxy/server.js
+++ weather-proxy/server.js
@@ -1,13 +1,17 @@
-import express from "express";                        //Framework for creating web servers
+import express from "express";                       //Framework for creating web servers
 import fetch from "node-fetch";                     //Allows us to use fetch in node.js
+import dotenv from "dotenv";                        //Environmental variable file fetch. Used for security and protecting sensitive infos like API keys.
+
+dotenv.config();                                    //Loading variables from env 
 
 const app = express();                            //Our server
-const PORT = 3000;                                //Server will be available at https://localhost:3000
+const PORT = process.env.PORT || 3000;            //Using available ports after checking in env.Server will be available at https://localhost:3000
+const apiKey = process.env.API_KEY;               //Read API_KEY from env
 
-// Simple endpoint: /weather?city=London
-app.get("/weather", async(req, res) => {
-  const { city } = req.query;                    //Setting up req query. For eg: we need to send "city: London" from frontend so it can be appended into the URL.
-  const apiKey = "935b90efd6ee4001db49960beac2fcdf";
+// Define route. Simple endpoint: /weather?city=London
+app.get("/weather", async(req, res) => {          //Execute the following function whenever https://localhost:3000/weather is ran
+  const { city } = req.query;                    //Gets city query from URL. For eg: we need to send "city: London" from frontend so it can be appended into the URL.
+//  const apiKey = "935b90efd6ee4001db49960beac2fcdf";
 
   try {
     const response = await fetch(
@@ -17,8 +21,11 @@ app.get("/weather", async(req, res) => {
     res.json(data);                             //Forward API response to frontend
   } catch (err) {
     console.error("Proxy error: ",err.message);
-    res.status(500).json({error: "Failed to fetch weather data"});  //Forward Internal Server Error msg to frontend
+    res.status(500).json({
+      error: "Failed to fetch weather data",    //Forward a safe and consistent error msg to frontend
+      details: err.message                      //We dont send this to frontend to keep the error msg consistent. Raw error may also contain sensitive infos so its not used.
+    });
   }
 });
 
-app.listen(PORT, () => console.log(`Proxy running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Proxy running at http://localhost:${PORT}`));     //Starts the server
