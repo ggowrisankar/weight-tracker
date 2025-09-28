354b598dd4e604090445de50581e02d39b0aad66
Gowri Sankar <gowrisankar9817@gmail.com>
2025-09-28 20:16:05 +0530

    Set up user schema with required fields.

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,37 @@ import express from "express";                      //Framework for creating web
 import cors from "cors";                            //Importing CORS to bypass restrictions
 import weatherRoutes from "./routes/weatherRoutes.js";
 import mongoose from "mongoose";                    //Imporitng mongoose library to interact with MongoDB
+import User from "./models/user.js";
+import bcrypt from "bcrypt";
 
 //Connecting the backend to MongoDB server
-mongoose.connect(process.env.MONGO_URI);            //Mongoose 6+ uses the new parser and unified topology by default.
-/*, {            //Configuration for the connection (uri, options):
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch(err => console.log("MongoDB connection error: ",err));;
+/*//Mongoose 6+ uses the new parser and unified topology by default.
+, {                                                  //Configuration for the connection (uri, options):
   useNewUrlParser: true,                             //uses the new URL parser (recommended by MongoDB)
   useUnifiedTopology: true                           //enables the new unified topology layer (improves connection handling & monitoring)
-})
-.then(() => console.log("Connected to MongoDB Atlas"))
-.catch(err => console.log("MongoDB connection error: ",err)); */
+});*/
 
 const app = express();                            //Our server
 const PORT = process.env.PORT || 3000;            //Using available ports after checking in env. Server will be available at https://localhost:3000 as default.
 
 app.use(cors());                                  //Now the server will be able to run anywhere without having to be inside the local url. (Keeping it open for now)
+
+/*//Test User code
+async function  testUser() {
+  try {
+    const hashedPassword = await bcrypt.hash("password123", 10);
+    const user = new User({ email: "test@example.com", passwordHash: hashedPassword });
+    await user.save();
+    console.log("Test user created");
+  }
+  catch (err) {
+    console.log("Test user creation error: ", err.message);
+  }
+}
+testUser();*/
 
 //Mount routes - prefix all paths inside weatherRoutes with /weather
 app.use("/weather", weatherRoutes);               //For any request starting with /weather, use the router imported from weatherRoutes.js
