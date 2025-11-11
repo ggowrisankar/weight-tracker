f9c2ac5c634767caa2feea9aec591e537c450318
Gowri Sankar <gowrisankar9817@gmail.com>
2025-11-11 18:08:48 +0530

    Mail sending functions are serviced using Brevo. Old SMTP related sendMail is skipped since Render can't connect directly to Gmail's SMPT server. Also fixed a small bug where user was receiving "password reset already requested" eventhough mail wasn't sent successfully.

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,13 +1,14 @@
 import nodemailer from "nodemailer";                      //Node.js library used to send emails
+import fetch from "node-fetch"; 
 
 export async function sendEMail(to, subject, html) {
   try {
+    /* [Skipping since backend can’t connect directly to Gmail’s SMTP server from Render]
     const transporter = nodemailer.createTransport({
-    /*Configuring host, port and secure for connecting to the smtp services. (Skipping since service: "gmail" autoconfigures it)
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: process.env.SMTP_SECURE === "true",   //Boolean check with strings
-    */
+    //Configuring host, port and secure for connecting to the smtp services. (Skipping since service: "gmail" autoconfigures it)
+      //host: process.env.SMTP_HOST,
+      //port: process.env.SMTP_PORT,
+      //secure: process.env.SMTP_SECURE === "true",   //Boolean check with strings
       service: "gmail",
       auth: {
         user: process.env.SMTP_USER,
@@ -24,6 +25,27 @@ export async function sendEMail(to, subject, html) {
     };
 
     await transporter.sendMail(mailOptions);             //Send the email
+    */
+    const response = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "POST",
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "api-key": process.env.BREVO_API_KEY
+      },
+      body: JSON.stringify({
+        sender: { name: "Keepr Weight Tracker", email: process.env.SMTP_USER },
+        to: [{ email: to }],
+        subject,
+        htmlContent: html
+      })
+    });
+
+    if(!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Email send failed: ${errorText}`);
+    }
+
     console.log(`Email sent to ${to}`);
   }
   catch (err) {
