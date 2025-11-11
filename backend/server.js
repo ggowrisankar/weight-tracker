8b6230fccce13499e66c4201679a50a1908f4a67
Gowri Sankar <gowrisankar9817@gmail.com>
2025-11-11 15:27:03 +0530

    Whitelisted the frontend origins with banckend to comply with CORS requirements.

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,51 @@ mongoose.connect(process.env.MONGO_URI)
 });*/
 
 const app = express();                              //Our server
-app.set("trust proxy", 1);                          //Enable trust for first proxy
+
+/* Tell Express that our app is running behind one reverse proxy (Render's load balancer). 
+This allows Express to trust the "X-Forwarded-*" headers that Render automatically adds to each incoming request. These headers contain important information such as:
+- X-Forwarded-For: the original client's IP address
+- X-Forwarded-Proto: the original request protocol (HTTP or HTTPS)
+
+By default, Express does NOT trust these headers because they could be spoofed if the app were directly exposed to the internet. However, since Render always sits between the client and our app, we can safely trust the first proxy (Render itself).
+This setting ensures that properties like req.ip, req.protocol, and req.secure
+correctly reflect the real client connection details instead of the proxy’s internal ones. */
+app.set("trust proxy", 1);
+
 const PORT = process.env.PORT || 3000;              //Using available ports after checking in env. Server will be available at https://localhost:3000 as default.
 
 app.use(express.json());                            //Middleware to parse JSON
 //app.use(express.urlencoded({ extended: true }));  //For accepting URL-encoded form data (form submissions)
 
-app.use(cors());                                    //Now the server will be able to run anywhere without having to be inside the local url. (Keeping it open for now)
+//Whitelist - an array of origins (URLs) that are allowed to access backend (these 2 are allowed send requests)
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://keepr-weight-tracker.vercel.app"
+];
+/*Applies the CORS middleware to every incoming request (middleware checks where the request is coming from and decides whether to allow it)
+The browser automatically attaches headers like:
+  Origin: https://keepr-weight-tracker.vercel.app
+  Credentials: <cookies if credentials included>
+The origin is then passed to an anonymous fn where callback is executed (handled by CORS library), which looks like:
+  callback(error: Error | null, allow?: boolean)
+*/
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      //Allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+
+      //Allow if origin is in the whitelist
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true
+  })
+);
 
 app.use(globalLimiter);                             //Apply global rate limiter to all routes (In prod, 429 error should be handled gracefully in frontend)
 
